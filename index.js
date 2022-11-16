43ea4674233f68b06a5f97572fc9d8485e2fe2f5
Grant Patterson <darkrider231@gmail.com>
2022-11-16 15:16:03 -0500

    Social Media Facebook Clone (not finished)

---
+++ index.js
@@ -0,0 +1,15 @@
+// Opens the Navigation Menu
+function openNav() {
+    document.getElementById("mySidenav").style.width = "25%";
+}
+
+// Closes the Navigation Menu
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0px";
+}
+
+
+// When user clicks submit button on the newsfeed post, it should be able to post it (not finished)
+function btnSubmit() {
+    
+}
